f61b0d51a9df2f8aa2231a67a429eabd8c0974d6
Amirhosseinsalimi123 <amir0905salimi@gmail.com>
2024-06-09 15:01:52 +0330

    make part of city model

--- core/area/models.py
+++ core/area/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    city = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.city
+    
+
+class Area(models.Model):
+    city = models.ForeignKey(to=City, on_delete=models.DO_NOTHING)
+    area = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.area
+
+
+class Community(models.Model):
+    area = models.ForeignKey(to=Area, on_delete=models.DO_NOTHING)
+    community = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.community
+
+class Part(models.Model):
+    community = models.ForeignKey(to=Community, on_delete=models.DO_NOTHING)
+    part = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.community
--- core/area/views.py
+++ core/area/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 from django.views.generic import DetailView
+
+
+from .models import City, Area, Community
 
 
 class CityProperties(DetailView):
     def get(self, request):
-        print(request.GET.get("city"))
+
+        city = request.GET.get("city", None)
+        area = request.GET.get("area", None) 
+        community = request.GET.get("community", None) 
+        part = request.GET.get("part", None) 
+
+        city, city_created = City.objects.get_or_create(city=city)
+
+        if area is not None:
+            area, area_created = Area.objects.get_or_create(area=area, city=city)
+            if community is not None:
+                community, community_created = Community.objects.get_or_create(area=area, community=community)
+
+        print(city)
+        print(area)
+        return HttpResponse("ok")
