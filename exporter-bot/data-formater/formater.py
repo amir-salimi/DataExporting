8ad29246e00f7cdd006a3850aabe547168106168
Amirhosseinsalimi123 <amir0905salimi@gmail.com>
2024-06-10 15:55:34 +0330

    get data with csv_creator from db and set in csv and format with formater.py

---
+++ exporter-bot/data-formater/formater.py
@@ -0,0 +1,11 @@
+#import modules
+import pandas as pd
+#reading the csv file
+emp = pd.read_csv('Data.csv')
+
+data = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+emp.head()
+emp.columns
+result = emp.drop_duplicates(["part"], keep="first")
+result.to_csv("new.csv")
