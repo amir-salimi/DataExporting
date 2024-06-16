b412aead0eb936f1a37e411fbe4f8ae804e8c04e
Amirhosseinsalimi123 <amir0905salimi@gmail.com>
2024-06-16 12:24:15 +0330

    move project exporter bot to this file

---
+++ exporter-bot/prj-exporter/dxb-offplan/dxb-scraping100.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+
+from help import remove_new_line
+
+import requests
+import time
+
+
+dxb_url = "https://dxboffplan.com/offplan-projects/"
+
+
+
+
+
+def data_from_url(link_array):
+
+    data_array = []
+
+    for link in link_array:
+        each_page = requests.get(link)
+        each_soup = BeautifulSoup(each_page.content, "html.parser")
+
+        if each_page.status_code == 429:
+            time.sleep(3)
+        try:
+            name = each_soup.find(class_="single-property-title").text
+            all_data = each_soup.find_all(class_="s-cl")
+            development = each_soup.find("p", class_="development").text
+            city = each_soup.find("p", class_="city").text
+            img = each_soup.find(class_="head-img").select_one("img").get('src')
+
+            for data in all_data:
+                val = remove_new_line(data.text)
+                data_array.append(val)
+            
+            price = data_array[0].replace(",", "")
+            price_per_meter = data_array[1].replace(",", "")
+
+            area = remove_new_line(data_array[2])
+            location = data_array[5]
+            developer = data_array[6]
+            developer_project_number = data_array[7]
+            handover = data_array[8]
+            views = data_array[9]
+            category = data_array[3]
+            bed_room = data_array[4]
+            bed_room = bed_room.split(".")
+            for bed in bed_room:
+                    requests.get(f"http://127.0.0.1:8000/dxb-data/?name={name}&development={development}&city={city}&img={img}&price={price}&price_per_meter={price_per_meter}&area={area}&category={category}&bed_room={bed}&views={views}&handover={handover}&developer_project_number={developer_project_number}&developer={developer}&location={location}&link={link}")
+        except:
+            pass
+
+        data_array = []
+    return 
+
+def get_link():
+    link_array = []
+    for i in range(146, 150): #233
+        print(i)
+        page = requests.get(dxb_url+f"/page/{i}/")
+        soup = BeautifulSoup(page.content, "html.parser")
+        for adv in soup.find_all(class_="card property-card"):
+            link = adv.select_one('a').get("href")
+            link_array.append(link)
+        data_from_url(link_array)
+
+
+get_link()
---
+++ exporter-bot/prj-exporter/opr-scraping.py
@@ -0,0 +1,193 @@
+from bs4 import BeautifulSoup
+import requests
+
+import time
+
+from help import remove_new_line, split_data, delete_repeat_data
+
+
+url = "https://opr.ae/projects"
+
+load_more_url = "https://api.opr.ae/offplan/"
+
+
+
+def get_each_project_approximate_location(soup):
+    data = ""
+    try:
+        location = soup.find(id="location").find_all(class_="zero-layer")
+        for i in location:
+            if i.text != "":
+                data += i.text + ", "
+        return data
+    except:
+        pass
+
+
+def get_bed_area_data(soup):
+    data_array = []
+    try:
+        floor_plans = soup.find(id="floor-plans").find_all(class_="swiper-slide")
+        for plan in floor_plans:
+            data = plan.find('p', class_="textable")
+            data = remove_new_line(data.text)
+            data_array.append(data)
+        s_data = split_data(data_array, ["Total:", "sqft"], ["-BedroomUnit"])
+        return s_data
+    except:
+        try:
+            floor_plans = soup.find(id="fp").find_all(class_="swiper-slide")
+            for plan in floor_plans:
+                data = plan.find('p', class_="textable")
+                data = remove_new_line(data.text)
+                data_array.append(data)
+            s_data = split_data(data_array, ["Total:", "sqft"], ["-BedroomUnit"])
+            return s_data
+        except:
+            pass
+
+
+def get_each_project_handover(soup):
+    array = []
+    handover = soup.find_all(class_="zero-layer-frame node-stretch")
+    for i in handover:
+        if i.text != "":
+            data = remove_new_line(i.text)
+            array.append(data)
+    return split_data(array, ["HANDOVER"], None)
+     
+
+def get_eahc_project_plan(soup, link):
+    plan_array = []
+    plan_data = soup.find_all(class_="swiper-slide")
+    for p in plan_data:
+        if p.select_one("img") != None:
+            plan_array.append(p.select_one("img").get("data-lazy-image"))
+    plan_map = delete_repeat_data(plan_array)
+    for i in plan_map:
+        requests.get(f"http://127.0.0.1:8000/opr-data/?link={link}&plan={i}")
+
+
+
+def get_each_project_img(soup, link):
+    imgs = soup.find_all(class_='gallery427-image fancybox')
+    for img in imgs:
+        image = img.get("href")
+        requests.get(f"http://127.0.0.1:8000/opr-data/?link={link}&img={image}")
+
+
+
+def get_each_project_about(soup, link):
+    try:
+        about = soup.find(id="about").find(class_="node").find_all(class_="col")
+        for i in about:
+            data = remove_new_line(i.text)
+            requests.get(f"http://127.0.0.1:8000/opr-data/?about={data}&link={link}")
+    except:
+        pass
+
+def get_each_project_frequently_question(soup, link):
+    array = []
+    try:
+        question = soup.find(id="questions-and-answers").find_all(class_="node")
+        for q in question:
+            data = q.text
+            data = data.replace("\n", "")
+            array.append(data)  
+        for i in array:
+            if i[::-1][:1] == "?":
+                requests.get(f"http://127.0.0.1:8000/opr-data/?question={i}&answer={array[array.index(i)+1]}&link={link}")
+    except:
+        pass
+
+def get_data_from_each_link(link, array):
+    each_page = requests.get(link)
+    each_soup = BeautifulSoup(each_page.content, "html.parser")
+
+    get_each_project_about(each_soup, link)
+
+    get_each_project_frequently_question(each_soup, link)
+
+    get_each_project_img(each_soup, link)
+
+    get_eahc_project_plan(each_soup, link)
+
+    bed_area = get_bed_area_data(each_soup)
+    
+    handover = get_each_project_handover(each_soup)
+
+    approximate_location = get_each_project_approximate_location(each_soup)
+    try:
+        for i in range(len(bed_area[0])):    
+            bed_room = bed_area[1][i]
+            area = bed_area[0][i]
+            area = area.replace(",", "")
+            HANDOVER = handover[0][0]
+            name = array[0]
+            location = array[1]
+            developer = array[2]
+            category = array[4]
+            price = array[5]
+            payment_plan = array[6]
+            status = array[7]
+            requests.get(f"http://127.0.0.1:8000/opr-data/?bed_room={bed_room}&handover={HANDOVER}&area={area}&name={name}&location={location}&developer={developer}&category={category}&link={link}&approximate_location={approximate_location}&price={price}&payment_plan={payment_plan}&status={status}")
+
+    except:
+        pass
+    
+
+def get_detail(soup):
+    array = []
+    name = soup.find(class_="offPlanListing__item-titleLink").text
+    location = soup.find(class_="offPlanListing__item-locations").text
+    developer = soup.find(class_="offPlanListing__item-developer").text
+    link = soup.find(class_="offPlanListing__item-blockLink").get("href")
+    category = soup.find(class_="offPlanListing__item-type").text
+    try:
+        price = soup.find(class_="offPlanListing__item-price").select_one("span").get("data-price")
+    except:
+        price = "Ask for"
+    try:
+        payment_plan = soup.find(class_="offPlanListing__stickers-item red").text
+    except:
+        payment_plan = None
+    try:
+        status = soup.find(class_="offPlanListing__stickers-item green").text
+    except:
+        status = None
+    
+    array = [name, location, developer, link, category, price, payment_plan, status]
+    value = get_data_from_each_link(link, array)
+    
+    
+
+
+def load_another_page():
+    for i in range(10, 14): #13
+        print(i)
+        loaded_page = requests.post(load_more_url, data={"page":f"{i}", "action":"getList", "language":"EN"})
+        loaded_soup = BeautifulSoup(loaded_page.content, "html.parser")
+        all_loaded_data = loaded_soup.find_all(class_="offPlanListing__item")
+        for soup in all_loaded_data:
+            time.sleep(1)
+            val = get_detail(soup)
+        
+
+            
+            
+
+def first_page_data():
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    all_data = soup.find_all(class_="offPlanListing__item")
+    for soup in all_data:
+        val = get_detail(soup)
+    load_another_page()
+
+
+
+first_page_data()
+
+
+
+
