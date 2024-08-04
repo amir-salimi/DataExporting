6ab72cf76972a73df58a498e479db516a2e99265
Amirhosseinsalimi123 <amir0905salimi@gmail.com>
2024-08-04 14:43:51 +0330

    add created def to building highlight and image and detail serializer model

--- core/Building/serializers.py
+++ core/Building/serializers.py
@@ -20,14 +20,40 @@ class BuildingHighlightModelSerializer(serializers.ModelSerializer):
         model = BuildingHighlight
         fields = "__all__"
 
+    def create(self, validated_data):
+        highlight, highlight_created = BuildingHighlight.objects.get_or_create(**validated_data)
+        return highlight
+
 
 class BuildingImgModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = BuildingImg
         fields = "__all__"
 
+    def create(self, validated_data):
+        image, image_created = BuildingImg.objects.get_or_create(**validated_data)
+        return image
+
 
 class BuildingDetailModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = BuildingDetail
         fields = "__all__"
+    
+    def create(self, validated_data):
+        detail, detail_created = BuildingDetail.objects.get_or_create(**validated_data)
+        return detail
+
+
+# class BuildingModelSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Building
+#         fields = [
+#             "city", "area", "community", "name", "source", "building_link", 
+#             "status", "location", "about", "publish_status", "details", 
+#             "img_link", "highlight"
+#         ]
+
+#     def create(self, validated_data):
+#         return super().create(validated_data)
+
